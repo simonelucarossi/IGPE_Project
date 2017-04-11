27986327d5ac2a327ba273f6ff217ed2f468c634
Simone Rossi <simone@protonmail.com>
2017-04-11 16:59:34 +0200

    add

--- IGPE_Project/src/Logic/AbstractTower.java
+++ IGPE_Project/src/Logic/AbstractTower.java
@@ -6,7 +6,7 @@ public class AbstractTower extends AbstractGameObject {
 		super(_position, _size);
 		// TODO Auto-generated constructor stub
 	}
-//
+//s
 	@Override
 	public boolean collide(ICollidable c) {
 		// TODO Auto-generated method stub
