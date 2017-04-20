96bf9bab920eddf36733711296a58c204a421e53
Giuseppe Pace <pace27.01@gmail.com>
2017-04-20 16:59:46 +0200

    new

--- IGPE_Project/src/Tower/AbstractTower.java
+++ IGPE_Project/src/Tower/AbstractTower.java
@@ -1,12 +1,52 @@
 package Tower;
 
+import Logic.ICollidable;
+import Logic.IGameObjectStatic;
 import Logic.Vector2d;
 
-public class AbstractTower extends AbstractGameObjectDynamic {
+public abstract class AbstractTower implements IGameObjectStatic{
+		
+		protected Vector2d position;
+		protected Vector2d size;
+		
+		public String toString() {
+			String s ="[";
+			s+=" pos: "+position.toString();
+			s+="]";
+			return s;
+		}
+		
+		public AbstractTower(Vector2d _position, Vector2d _size) {
+			this.position = _position;
+			this.size = _size;
+		}
+		
+		@Override
+		public Vector2d getPosition() {
+			return position;
+		}
+		
+		@Override
+		public Vector2d getSize() {
+			return size;
+		}
+		
+		@Override
+		public void setPosition(Vector2d pos) {
+			position = pos;
+			
+		}
 
-	public AbstractTower(Vector2d _position, Vector2d _size) {
-		super(_position, _size);
-		// TODO Auto-generated constructor stub
+		@Override
+		public void setSize(Vector2d _size) {
+			this.size = _size;
+			
+		}	
+		
+		@Override
+		public boolean collide(ICollidable c) {
+			
+			return false;
+		}	
+
 	}
-	
-}
